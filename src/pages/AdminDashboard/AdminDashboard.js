18171ffd3fcf1d94d22ba9624120b931fdbdaa6e
sendistephen <sendi.steve2@gmail.com>
2021-11-07 10:54:02 +0300

    Improve Admin user dashboard

--- src/pages/AdminDashboard/AdminDashboard.js
+++ src/pages/AdminDashboard/AdminDashboard.js
@@ -1,6 +1,5 @@
-import { isAuthenticated } from 'api/auth';
-import { Layout } from 'components';
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from 'api/auth';
 
 const AdminDashboard = () => {
   const {
@@ -8,55 +7,57 @@ const AdminDashboard = () => {
   } = isAuthenticated();
 
   const adminLinks = () => (
-    <div className='card'>
-      <h5 className='card-header'>Admin Links</h5>
-      <ul className='list-group'>
-        <li className='list-group-item'>
+    <>
+      <h5 className='font-bold my-4'>Admin Links</h5>
+      <div className='bg-white rounded-lg border border-gray-200 text-gray-900 font-medium text-sm mb-4'>
+        <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg cursor-pointer'>
           <Link className='nav-link' to='/admin/category/create'>
             Create Category
           </Link>
         </li>
-        <li className='list-group-item'>
+        <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg cursor-pointer'>
           <Link className='nav-link' to='/admin/product/create'>
             Create Product
           </Link>
         </li>
-        <li className='list-group-item'>
+        <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg cursor-pointer'>
           <Link className='nav-link' to='/admin/orders'>
             View Orders
           </Link>
         </li>
-        <li className='list-group-item'>
+        <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg cursor-pointer'>
           <Link className='nav-link' to='/admin/products'>
             Manage Products
           </Link>
         </li>
-      </ul>
-    </div>
+      </div>
+    </>
   );
 
   return (
-    <Layout
-      className='container'
-      title='Admin Dashboard'
-      description={`Hey ${name}!`}
-    >
-      <div className='row'>
-        <div className='col-md-3'>{adminLinks()}</div>
-        <div className='col-md-9'>
-          <div className='card mb-5'>
-            <h5 className='card-header'>Admin Information</h5>
-            <ul className='list-group'>
-              <li className='list-group-item'>{name}</li>
-              <li className='list-group-item'>{email}</li>
-              <li className='list-group-item'>
-                {role === 1 ? 'Admin' : 'Registered User'}
-              </li>
-            </ul>
-          </div>
+    <div className='px-8 mt-12'>
+      <div className='bg-white px-8 py-2 rounded'>
+        <h1 className='text-2xl'>Admin Dashboard</h1>
+        <span className='text-sm text-purple-800'>{`Hey ${name}!`}</span>
+      </div>
+      <div className='bg-white px-8 py-2 rounded mt-4 flex'>
+        <div className='w-48'>{adminLinks()}</div>
+        <div className='ml-24 w-full '>
+          <h4 className='font-bold mt-4'>Admin Information</h4>
+          <ul className='font-medium border border-gray-200 rounded-lg mt-4 text-sm'>
+            <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg '>
+              {name}
+            </li>
+            <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg '>
+              {email}
+            </li>{' '}
+            <li className='block px-4 py-2 border-b border-gray-200 w-full rounded-t-lg '>
+              {role === 1 ? 'Admin' : 'Registered User'}{' '}
+            </li>{' '}
+          </ul>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 };
 
